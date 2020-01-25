3e9a7ddb9aa4a42f6ae585b3e4f64571a1c46d78
Alessio Raimondi <alessio.raimondi@mail.polimi.it>
2020-01-25 10:34:33 +0100

    Fixed loglog plot

--- script_matlab/confronto.m
+++ script_matlab/confronto.m
@@ -1,12 +1,14 @@
 % Confronto
 close all; clear; clc
 
+addpath('LiteratureModels')
+
 % Implemented, working models
 models = ["Holzer&Sommerfeld", "Holzer&Sommerfeld_simplified", ...
           "Swmee&Ojha", "Ganser", "Chien", "Dioguardi"];
 
 % Implemented, working geometries
-geometries = ["sphere", "cube", "spherocylinder"];
+geometries = ["sphere", "cube", "cylinder", "spherocylinder"];
 
 % Parameters
 r = 1;              % radius for sphere, spherocylinder
@@ -14,20 +16,20 @@ l = 1;              % length for cube
 h = 5;              % height for spherocylinder
 D = inf;            % tube diameter (Ganser)
 
-Re = logspace(1, 3, 1000);
+Re = logspace(-3, 1, 1000);
 cD = zeros(1, length(Re));
 
 for k = 1:1:length(geometries)
     param = ShapeParameters (models, geometries(k), 1, 1, 5, inf);
 
     figure('Name', geometries(k))
-    hold on
     Legend = cell(1, length(models));
     for i = 1:1:length(models)
         for j = 1:1:length(Re)
             cD(j) = cD_model(Re(j), models(i), param);
         end
-        semilogx(Re, cD)
+        loglog(Re, abs(cD))
+        hold on
         Legend{i} = models(i);
     end
     legend(Legend)
